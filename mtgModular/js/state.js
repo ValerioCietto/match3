2bbde9ed92477b7a58344ddc48f3ffe8e1407c13
Valerio Cietto <valerio_ciet@hotmail.it>
2025-07-16 00:31:14 +0200

    Create state.js

---
+++ mtgModular/js/state.js
@@ -0,0 +1,25 @@
+export const CARD_LIBRARY = {};
+export const composition = {};
+
+export let state = {
+  turn: 0,
+  life: 20,
+  deck: [],
+  hand: [],
+  battlefield: [],
+  graveyard: [],
+  landsPlayed: 0,
+  gameEnded: false,
+  selectedCardIndex: null
+};
+
+export function saveState() {
+  localStorage.setItem('mtgSimulatorState', JSON.stringify(state));
+}
+
+export function loadState() {
+  const saved = localStorage.getItem('mtgSimulatorState');
+  if (saved) {
+    Object.assign(state, JSON.parse(saved));
+  }
+}
