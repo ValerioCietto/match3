9fd44170d6acf01a547fea6c63d48be8ea7bd13d
Valerio Cietto <valerio_ciet@hotmail.it>
2025-07-16 09:26:37 +0200

    Update cardsLoader.js

--- mtgModular/js/cardsLoader.js
+++ mtgModular/js/cardsLoader.js
@@ -1,7 +1,7 @@
 import { CARD_LIBRARY, composition } from './state.js';
 
 export async function loadCards() {
-  const res = await fetch('../cards.json');
+  const res = await fetch('../mtgModular/cards.json');
   const data = await res.json();
 
   Object.assign(CARD_LIBRARY, data.library);
