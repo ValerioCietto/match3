80ad6de956896d2c21b7aaa58775d8fd15bbf588
Valerio Cietto <siluxmedia@gmail.com>
2025-08-01 20:21:07 +0200

    Update cardsLoader.js

--- mtgModular/js/cardsLoader.js
+++ mtgModular/js/cardsLoader.js
@@ -1,7 +1,7 @@
 import { CARD_LIBRARY, composition } from './state.js';
 
 export async function loadCards() {
-  const res = await fetch('../mtgModular/cards.json');
+  const res = await fetch('../mtgModular/cards_tifa_landfall.json');
   const data = await res.json();
 
   Object.assign(CARD_LIBRARY, data.library);
